78ecb11f9b857b60b3bdfc5c7c01dccfa093fe39
Divyansh Nakhate <138779643+divyansh02n@users.noreply.github.com>
2023-07-21 22:30:09 +0530

    Create 21-July.cpp

---
+++ 21-July.cpp
@@ -0,0 +1,24 @@
+class Solution
+{
+    public:
+    struct node* reverse(struct node* head, int k) {
+        if (head == nullptr) return nullptr;
+        
+        struct node* prev = nullptr;
+        struct node* next = nullptr;
+        struct node* headptr = head;
+        int cnt = k;
+        
+        while (headptr != nullptr && cnt != 0) {
+            next = headptr->next;
+            headptr->next = prev;
+            prev = headptr;
+            headptr = next;
+            cnt--;
+        }
+        
+        head->next = reverse(headptr, k);
+        
+        return prev;
+    }
+};
